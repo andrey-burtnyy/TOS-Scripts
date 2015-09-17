d75f122875e097dc7b0db06fb296c0d679e0475d
Andrey Burtnyy <andreybass1@gmail.com>
2015-09-17 18:12:44 +0300

    Update TOS-LevelFinder.ts
    Added setting: only rounded levels

--- TOS-LevelFinder.ts
+++ TOS-LevelFinder.ts
@@ -22,12 +22,14 @@
 def sMinATR = 0.5; # Min ATR
 def sMaxATR = 5; # Max ATR
 def sMaxPassedATR = 80; # Max passed Current ATR; In %
+def sMinPassedATR = 0; # Min passed Current ATR; In %
 def sMinPrice = 20; # Min stock price
 def sMaxPrice = 135; # Max stock price
 def sGlobalTrendLength = 1; # Check for trend for N days. 1 = off
 def sLocalTrendEnable = 0; # Check for local trend. 0 = off, 1 = on
-def sDiff = 2; # Diff of the pre-last bar in cents;
+def sDiff = 1; # Diff of the pre-last bar in cents;
 def sDiffForATRHigher = 1.5; # enable diff for ATR higher than X; 0 - for any.
+def sOnlyRounded = 1; # Only rounded levels, >0.9 && <0.1 || ==0.5
 
 def highLevel;
 def lowLevel;
@@ -47,16 +49,22 @@ def MA = if sGlobalTrendLength > 1 then
                         ) else 0;
 def trendGlobal = if sGlobalTrendLength > 1 then MA[1] - MA[sGlobalTrendLength] else 0;
 
-if ((ATR >= sMinATR and ATR <= sMaxATR and currentATR <= (ATR * sMaxPassedATR / 100)) and
+def high2 = high[2];
+def low2 = low[2];
+
+if ((ATR >= sMinATR and ATR <= sMaxATR and currentATR >= (ATR * sMinPassedATR / 100) and currentATR <= (ATR * sMaxPassedATR / 100)) and
     (close[0] >= sMinPrice and close[0] <= sMaxPrice)) {
 
     # SHORT
     if (sGlobalTrendLength <= 1 or trendGlobal < 0) and # GLOBAL TREND
         (sLocalTrendEnable == 0 or close[0] <= open(period = "DAY")[0]) and # LOCAL TREND
-        ((ATR >= sDiffForATRHigher and (high[2] - high[1] >=0 and high[2] - high[1] <= sDiff / 100)) or high[2] == high[1]) and
-        high[0] <= high[1] {
-        highLevel = fold i = 3 to barsCount + 1 with p = 0 while p <= 0 do
-                    if (high[i] == high[2] or low[i] == high[2]) then high[2] else 0;
+        ((ATR >= sDiffForATRHigher and (high2 - high[1] >=0 and high[2] - high[1] <= sDiff / 100)) or high2 == high[1]) and
+        high[0] <= high[1] 
+        and (high2%1>0.9 or high2%1<0.1 or high2%1==0.5) 
+       # and high[2] == high[3]
+        {
+        highLevel = fold i = 3+1 to barsCount + 1 with p = 0 while p <= 0 do
+                    if (high[i] == high2 or low[i] == high2) then high2 else 0;
     } else {
         highLevel = 0;
     }
@@ -64,10 +72,13 @@ if ((ATR >= sMinATR and ATR <= sMaxATR and currentATR <= (ATR * sMaxPassedATR / 
     # LONG
     if (sGlobalTrendLength <= 1 or trendGlobal > 0) and # GLOBAL TREND
         (sLocalTrendEnable == 0 or close[0] >= open(period = "DAY")[0]) and # LOCAL TREND
-        ((ATR >= sDiffForATRHigher and (low[1] - low[2] >= 0 and low[1] - low[2] <= sDiff / 100)) or  low[1] == low[2]) and
-        low[0] >= low[1] {
-        lowLevel = fold i2 = 3 to barsCount + 1 with p2 = 0 while p2 <= 0 do
-                    if (low[i2] == low[2] or high[i2] == low[2]) then low[2] else 0;
+        ((ATR >= sDiffForATRHigher and (low[1] - low2 >= 0 and low[1] - low2 <= sDiff / 100)) or  low[1] == low2) and
+        low[0] >= low[1] 
+        and (low2%1>0.9 or low2%1<0.1 or low2==0.5) 
+       # and low[2] == low[3] 
+        {
+        lowLevel = fold i2 = 3+1 to barsCount + 1 with p2 = 0 while p2 <= 0 do
+                    if (low[i2] == low2 or high[i2] == low2) then low2 else 0;
     } else {
         lowLevel = 0;
     }
